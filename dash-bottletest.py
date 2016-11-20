65303b92be283d77e7b3d9737ebc61d014de8981
Alex <alex@waibel.us>
2016-11-19 19:23:39 -0500

    Added support for multiple phone numbers
    Added support for the program to send the alert to multiple phone
    numbers.

--- dash-bottletest.py
+++ dash-bottletest.py
@@ -1,8 +1,11 @@
 import sendText
-from bottle import route, run, template
-
+from bottle import route, run, template, request
+number = "0"
 @route('/hello')
 def index():
     sendText
 
+@route('/number')
+def index():
+	number = request.getone('number')
 run(host='localhost', port=8080)
