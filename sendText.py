2b4db7570af0f8ed78ba21a11499f3acd01c8964
Alex <alexwaibel@users.noreply.github.com>
2016-11-19 20:04:40 -0500

    Hopefully fixed bypassing button entirely.
    The program used to just send a text out the moment it started. Hopefully this fixes that... maybe

--- sendText.py
+++ sendText.py
@@ -15,5 +15,6 @@ def sendMessage(sendToNumber):
     print(message.sid)
 
 
-for number in phoneNumbers:
-    sendMessage(number)
+def sendItOutPlz():
+    for number in phoneNumbers:
+        sendMessage(number)
